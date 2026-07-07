65dd9a7197228b345d821fb9172314cabb217fc2
Rickard Falk <ricf@kth.se>
2026-07-07 07:34:58 +0200

    chore(deps-dev): bump dotenv to v17

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-require('dotenv').config()
+require('dotenv').config({ quiet: true })
 
 const { KthAppinsights } = require('@kth/appinsights')
 KthAppinsights.init({ name: 'node-api' })
